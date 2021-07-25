0dbdc18a7aedd56a6563abc71dca1a2219e6f2cd
Cemal Yetismis <cemal.yetismis@student.kuleuven.be>
2021-07-25 15:01:44 +0200

    LeetCode exercise 58

---
+++ src/com/company/PlusOne.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int [] digits={9,9};
+
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+    public static int[] plusOne(int[] digits) {
+        int i=0;
+        if (digits[digits.length-1]==9){
+            for (i = digits.length-1; i>=0; i--) {
+                if (digits[i]==9){
+                    digits[i]=0;
+                }else{
+                    digits[i]+=1;
+                    break;
+                }
+
+            }
+        }else{
+            digits[digits.length-1]=digits[digits.length-1]+1;
+        }
+        if (i == -1){
+            int [] newArray=new int[digits.length+1];
+            newArray[0]=1;
+            for (int j = 0; j < digits.length ; j++) {
+                newArray[j+1]=digits[j];
+                return newArray;
+            }
+        }
+        return digits;
+    }
+}
