bf07ff0aad4075965328ad6a5122c9e0069c89b7
Cemal Yetismis <cemal.yetismis@student.kuleuven.be>
2021-07-28 13:23:43 +0200

    LeetCode exercise 67

--- src/com/company/AddBinary.java
+++ src/com/company/AddBinary.java
@@ -4,27 +4,33 @@ import java.util.Arrays;
 
 public class AddBinary {
     public static void main(String[] args) {
-        String a = "001"; String b = "1110";
+        String a = "11011"; String b = "1";
         //addBinary(a,b);
         System.out.println(addBinary(a,b));
     }
     public static String addBinary(String a, String b) {
-        int shortString=b.length()-1;
-        int longString=a.length()-1;
-        String result="";
+        String shortString=a;
+        String longString=b;
+        StringBuilder result= new StringBuilder();
         int carry=0;
-        if (a.length()<b.length()){
-            shortString=a.length()-1;
-            longString=b.length()-1;
+        if (a.length()>b.length()){
+            shortString=b;
+            longString=a;
         }
-        for (int i = shortString; i >=0 ; i--) {
-            System.out.println((Character.getNumericValue(a.charAt(i))+Character.getNumericValue(b.charAt(i))+carry)%2);
-            result+=(Character.getNumericValue(a.charAt(i))+Character.getNumericValue(b.charAt(i))+carry)%2;
-            carry=(Character.getNumericValue(a.charAt(i))+Character.getNumericValue(b.charAt(i))+carry)/2;
+        int j=shortString.length()-1;
+        int i=longString.length()-1;
+        for (; i >=0 ; i--,j--) {
+            if (j>=0){
+                result.append((Character.getNumericValue(longString.charAt(i)) + Character.getNumericValue(shortString.charAt(j)) + carry) % 2);
+                carry=(Character.getNumericValue(longString.charAt(i))+Character.getNumericValue(shortString.charAt(j))+carry)/2;
+            }else if (carry==1){
+                result.append((Character.getNumericValue(longString.charAt(i)) + carry) % 2);
+                carry=(Character.getNumericValue(longString.charAt(i))+carry)/2;
+            }else{
+                return longString.substring(0,i+1)+result.reverse();
+            }
+
         }
-
-
-        //System.out.println(result);
-        return carry == 1 ? result + carry : result;
+        return carry == 1 ? carry + result.reverse().toString() : result.reverse().toString();
     }
 }
